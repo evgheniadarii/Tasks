e88cc13d168fb82610515058674d877732cc1370
evgheniadarii <evgheniadarii22@gmail.com>
2023-06-23 12:43:20 -0500

    checked and finished!

---
+++ src/LeetCode/SumOfThreeNumbers.java
@@ -0,0 +1,21 @@
+package LeetCode;
+
+import java.util.Scanner;
+
+public class SumOfThreeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Enter the second number:");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Enter the third number:");
+        int num3 = scanner.nextInt();
+
+        int sum = num1 + num2 + num3;
+        System.out.println("The sum of the three numbers is: " + sum);
+    }
+}
